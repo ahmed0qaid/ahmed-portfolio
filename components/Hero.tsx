439fb373268989f84078a141d891796baf2ab7ad
AHMED QAID <ahmed0qaid@gmail.com>
2026-09-02 13:17:27 +0300

    Remove combined Arabic backend-cloud title

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,7 +26,6 @@ export function Hero({ cvDownloadEnabled, content, contentEn, layoutVariant }: H
   const isCompact = layoutVariant === "compact";
   const isShowcase = layoutVariant === "showcase";
   const arDynamicTitles = [
-    "مهندس برمجيات باك إند وسحابي",
     "مهندس أنظمة باك إند",
     "مهندس أنظمة سحابية",
     "مهندس منصات برمجية",
